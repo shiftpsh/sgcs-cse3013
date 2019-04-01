5ab5c74cf67e9dc83388643c41378a703093b2a3
Suhyun Park <shift@v.anil.la>
2019-04-01 22:14:11 +0900

    Update utol.sh

--- 02-unix/unix-1/utol.sh
+++ 02-unix/unix-1/utol.sh
@@ -1,19 +1,19 @@
 echo "Working directory:"
-read dir                # Read directory from user
+read dir                        # Read directory from user
 
-if [ $dir ]				# If input is given:
+if [ $dir ]                     # If input is given:
 then
-	cd $dir	            # Move to given directory
-	if [ $? -ne 0 ]	    # If cd fails: exit
+	cd $dir                 # Move to given directory
+	if [ $? -ne 0 ]	        # If cd fails: exit
 	then
 		echo "Error: Cannot access directory, check permissions"
 		exit 0
 	fi
 fi
 
-for file in *			# Loop for all files/directories in dir
+for file in *                   # Loop for all files/directories in dir
 do
-	if [ -f $file ]		# If $file is file
+	if [ -f $file ]         # If $file is file
 	then
 		mv $file `echo $file | tr '[A-Z][a-z]' '[a-z][A-Z]'`	
 	fi
