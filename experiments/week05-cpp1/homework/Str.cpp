355545ae2e49d358bd295f2adfec983ee6ae2384
Suhyun Park <me@shiftpsh.com>
2019-04-14 18:01:34 +0900

    Finished homework for CPP1

---
+++ experiments/week05-cpp1/homework/Str.cpp
@@ -0,0 +1,55 @@
+#include "Str.h"
+#include <cstring>
+#include <cstdlib>
+
+Str::Str(int length) {
+    len = length;
+    str = new char[len + 1];
+    str[0] = '\0';
+}
+
+Str::Str(char* contents) {
+    len = strlen(contents);
+
+    str = new char[len + 1];
+    strcpy(str, contents);
+    str[len] = '\0';
+}
+
+Str::~Str() {
+    delete []str;
+}
+
+int Str::length() {
+    return len;
+}
+
+char *Str::contents() {
+    return str;
+}
+
+int Str::compare(char *a) {
+    return strcmp(str, a);
+}
+
+int Str::compare(Str &a) {
+    return (*this).compare(a);
+}
+
+void Str::operator=(char *a) {
+    int a_len = strlen(a);
+
+    if (len < a_len) {
+		delete []str;
+		str = new char[a_len + 1];
+	}
+
+    len = a_len;
+
+    strcpy(str, a);
+    str[len] = '\0';
+}
+
+void Str::operator=(Str &a) {
+    (*this) = a.contents();
+}
